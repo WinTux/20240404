a9b456b8fc51f568c91573c8234a8b87665abb21
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2024-04-10 20:27:37 -0400

    Ejemplo DrawingView final

--- 20240404/Pages/EjemploTrazos.xaml.cs
+++ 20240404/Pages/EjemploTrazos.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui.Core;
+
 namespace _20240404.Pages;
 
 public partial class EjemploTrazos : ContentPage
@@ -5,5 +7,12 @@ public partial class EjemploTrazos : ContentPage
 	public EjemploTrazos()
 	{
 		InitializeComponent();
+	}
+	async void OnLineaDibujada(object sender, DrawingLineCompletedEventArgs e)
+	{
+		var stream = await lienzo.GetImageStream(200, 200);
+		Dispatcher.Dispatch(() => {
+			imagen.Source = ImageSource.FromStream(() => stream);
+		});
 	}
 }
